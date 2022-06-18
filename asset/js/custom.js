c47aa556aa7994bc54d567143762eb6b443fb67e
jiyoonjang <rchel02@naver.com>
2022-06-18 19:42:22 +0900

    전체 수정

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,10 +1,10 @@
 $(function(){
   /**
- * 주석달아주기
+ * 메뉴바 상단고정
  *
  * @version 1.0.0
- * @since 2022-01-16
- * @author 본인이름 (Nico)
+ * @since 2022-06-18
+ * @author jy
  */
 
   // 
@@ -13,37 +13,38 @@ $(function(){
     const curr=$(window).scrollTop();
     if (curr>0) {
       $('.header-inner').addClass('fixed')
+      $('.submenu-area').css('visibility', 'visible');
     } else {
       $('.header-inner').removeClass('fixed')
+      $('.submenu-area').css('visibility', 'hidden');
     }
     
   })
 
-
-    /**
-   * 주석달아주기
-   *
-   * @version 1.0.0
-   * @since 2022-01-16
-   * @author 본인이름 (Nico)
-   */
-  
+/**
+ * 메뉴 열고닫기
+ *
+ * @version 1.0.0
+ * @since 2022-06-18
+ * @author jy
+ */
     // 
     $('.header .menu-area .link-more').click(function(e){
       e.preventDefault();
       $('.header .submenu-area').stop().slideToggle();
+      $('.header .menu-area .link-more').toggleClass('active');
     })
 
 
  /**
-   * 주석달아주기
-   *
-   * @version 1.0.0
-   * @since 2022-01-16
-   * @author 본인이름 (Nico)
-   */
-  
-    // 
+  * 메뉴펼치면 배경 어두워지게
+  *
+  * @version 1.0.0
+  * @since 2022-06-18
+  * @author jy
+  */
+
+ // 
 
     $('.btn-menu').click(function (e) {
       e.preventDefault();
@@ -57,14 +58,13 @@ $(function(){
       $('body').removeClass('hidden');
     })
 
-
-    /**
-   * 주석달아주기
-   *
-   * @version 1.0.0
-   * @since 2022-01-16
-   * @author 본인이름 (Nico)
-   */
+/**
+ * 메뉴 열고닫기
+ *
+ * @version 1.0.0
+ * @since 2022-06-18
+ * @author jy
+ */
   
     // 
     $('.gnb-area .btn-category').click(function(e){
@@ -75,12 +75,12 @@ $(function(){
     })
     
     /**
-   * 스크롤이벤트
-   *
-   * @version 1.0.0
-   * @since 2022-01-16
-   * @author 본인이름 (Nico)
-   */
+     * 톡상담 영역 스크롤 내리면 사라지고 올리면 나타나게
+     *
+     * @version 1.0.0
+     * @since 2022-06-18
+     * @author jy
+     */
   
     // 
     let last=0;
@@ -100,7 +100,14 @@ $(function(){
 
 })
 
-
+ /**
+  * 맨윗부분 슬라이드
+  *
+  * @version 1.0.0
+  * @since 2022-06-18
+  * @author jy
+  */
+ // 
 var swiper = new Swiper(".top-slide", {
   autoplay: {
       delay: 1500,
@@ -111,13 +118,14 @@ var swiper = new Swiper(".top-slide", {
 });
 
 
-
-var swiper = new Swiper(".product-slide", {
-    slidesPerView : '2.2',
-    spaceBetween: 10,
-    freeMode : true,
-});
-
+ /**
+  * 메인부분 슬라이드
+  *
+  * @version 1.0.0
+  * @since 2022-06-18
+  * @author jy
+  */
+ // 
 var swiper = new Swiper(".main-slide", {
     pagination: {
       el: ".swiper-pagination",
@@ -130,112 +138,17 @@ var swiper = new Swiper(".main-slide", {
     effect : 'fade',
   });
 
+   /**
+  * 상품 슬라이드
+  *
+  * @version 1.0.0
+  * @since 2022-06-18
+  * @author jy
+  */
+ // 
 
-
-
-// $(window).on('mousewheel',function(e){
-//   var wheel = e.originalEvent.wheelDelta;
-  
-//   if(wheel>0){
-//     $('.wrap').addClass('active');
-//     $('.wrap').removeClass('no');
-    
-//   } else {
-//     $('.wrap').addClass('no');
-//     $('.wrap').removeClass('active');
-      
-//   }
-//   });
-
-
-
-// ==================리스트내리고올리는거
-  // $('.category-wrap .btn-category').click(function(e){
-  //   e.preventDefault();
-  //   $('.category-wrap .sub-list').stop().slideUp();
-  // },function(){
-  //   $('.category-wrap .sub-list').stop().slideDown();
-  // });
-
-  // $('.btn-category').click(function(e){
-  //   e.preventDefault();
-  //   $('.sub-list').stop().slideUp();
-  //   $(this).siblings().stop().slideToggle();
-  // });
-
-  // =================================
-
-
-
-
-
-  
-
-  
-
-
-
-  // $(window).scroll(function(){
-  //   curr=$(window).scrollTop();
-  //   target=$('.logo-area').offset().top;
-
-  //   if(curr >= target){
-  //     $('.logo-wrap').show();
-  //     $('.header .swiper-wrapper').show();
-  //     $('logo-area-fixed').hide();
-  //   }else{
-  //     $('.logo-wrap').hide();
-  //     $('.header .swiper-wrapper').hide();
-  //     $('.logo-area-fixed').show();
-  //   }
-  // })
-
-
-  // $(window).scroll(function(){
-  //   curr=$(window).scrollTop();
-  //   target=$('.logo-area').offset().top;
-
-  //   if(curr >= target){
-  //     $('.logo-wrap').css('display', 'block');
-  //     $('.header .swiper-wrapper').css('display', 'block');
-  //     $('logo-area-fixed').css('display', 'none');
-  //   }else{
-  //     $('.logo-wrap').css('display', 'none');
-  //     $('.header .swiper-wrapper').css('display', 'none');
-  //     $('.logo-area-fixed').css('display', 'block');
-  //     $('.logo-area-fixed').css('top', '0');
-  //   }
-  // })
-
-
-  // $( window ).ready( function() {
-  //   var menuOffset = $( '.logo-area-fixed' ).offset();
-  //   $( window ).scroll( function() {
-  //     if ( $( window ).scrollTop() > menuOffset.top ) {
-  //       $( '.logo-area-fixed' ).addClass( 'logo-fixed' );
-  //     }
-  //     else {
-  //       $( '.logo-area-fixed' ).removeClass( 'logo-fixed' );
-  //     }
-  //   });
-  // } );
-
-
-  
-  // $(document).ready(function () {
-  //       var topBar = $(".logo-wrap").offset();
-  //       $(window).scroll(function () {
-  //           var docScrollY = $(document).scrollTop()
-  //           var barThis = $(".logo-wrap")
-  //           var fixNext = $(".logo-area-fixed")
-  //           if (docScrollY > topBar.top) {
-  //             barThis.addClass("logo-fixed");
-  //             fixNext.addClass("pd_top_80");
-  //           } else {
-  //             barThis.removeClass("logo-fixed");
-  //             fixNext.removeClass("pd_top_80");
-  //           }
-  //           });
-  //       })
-
-  
+var swiper = new Swiper(".product-slide", {
+  slidesPerView : '2.2',
+  spaceBetween: 10,
+  freeMode : true,
+});
